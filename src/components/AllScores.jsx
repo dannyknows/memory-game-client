865a5d2fc39a36a774849fce41599bede58efdfa
GorginZ <georgia.z.leng@gmail.com>
2020-07-08 21:33:48 +1000

    AllScores component to render rails api score data

---
+++ src/components/AllScores.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+//get scores from rails
+class AllScores extends React.Component {
+  state = { scores: [] };
+
+  getScores = async () => {
+    const response = await fetch("http://localhost:3000/scores");
+    const scores = await response.json();
+    this.setState({ scores: scores });
+  };
+
+  renderScores = () => {
+    return this.state.scores.map((score, index) => {
+      return (
+        <div key={index} className="score">
+          <h3>{score.name}</h3>
+          <h4>Score: </h4>
+          <p>{score.score}</p>
+          <hr />
+        </div>
+      );
+    });
+  };
+  async componentDidMount() {
+    this.getScores();
+  }
+
+  render() {
+    return <div>{this.renderScores()}</div>;
+  }
+}
+
+export default AllScores;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/App.css";
 import Grid from "./grid";
+import AllScores from "./AllScores";
 
 function App() {
   return (
     <>
       <h1>hello world</h1>
       <Grid/>
+      <AllScores/>
     </>
   );
 }
