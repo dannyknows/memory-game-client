e94a9aa10c54de577cd4e791ca531eee0769c0c6
Danny_knows <dannylow456@gmail.com>
2020-07-09 13:46:07 +1000

    current changes, working on visible trackng

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,21 @@ import React, { Component } from "react";
 import "../styles/App.scss";
 import Grid from "./grid";
 import AllScores from "./AllScores";
+import visArray from "./visArray";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { clicks: 0, visible: [] };
+    // array tracking wip
+    this.state = { clicks: 0, visArray: [] };
   }
-  visArray = (tileId) => {
-    console.log("in vis array");
-    this.setState((s) => {
-      if (s.visible.length === 2) {
-        s.visible.shift();
-        s.visible.push(tileId);
-      } else {
-        s.visible.push(tileId);
-      }
-    });
-    console.log(this.state.visible);
-  };
 
   handleClick = (event) => {
     console.log(event.target.id);
     event.target.style = "background-color: greenyellow";
     console.log(event.target);
-    this.visArray(event.target.id);
+    // array tracking wip
+    // visArray(event.target, this.setState((s, c)=>{this.setState(s, c)}))
     this.setState((state) => {
       return (state.clicks += 1);
     });
@@ -38,7 +29,7 @@ class App extends Component {
       <>
         <h1>hello world</h1>
         <Grid tileEvent={this.handleClick} />
-        <AllScores/>
+        <AllScores />
       </>
     );
   }
