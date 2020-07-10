cea79e91082138f8d0f9ca0328ba02bf42b3d90f
GorginZ <georgia.z.leng@gmail.com>
2020-07-10 14:49:55 +1000

    nav bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,18 @@ import "../styles/App.scss";
 import AllScores from "./AllScores";
 import Game from "./game";
 import Landing from "./landing";
+import NavBar from "./NavBar";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/play" component={Game} />
-          <Route exact path="/scoreboard" component={AllScores} />
-        </Switch>
+        <NavBar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/play" component={Game} />
+            <Route exact path="/scoreboard" component={AllScores} />
+          </Switch>
       </>
     );
   }
